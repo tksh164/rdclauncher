3552e8a55443501564be04cfc13f71b93d5f42ea
Takeshi Katano <takatano@outlook.com>
2021-03-14 20:04:21 +0900

    Fix app window title

--- src/msrdcui/WindowTitleBuilder.cs
+++ src/msrdcui/WindowTitleBuilder.cs
@@ -6,7 +6,7 @@ namespace rdclauncher
     {
         public static string GetWindowTitle()
         {
-            const string WindowTitle = "Remote Desktop Client Launcher";
+            const string WindowTitle = "RDC Launcher";
             var version = Assembly.GetEntryAssembly().GetName().Version;
             string versionText = string.Format("v{0}.{1}.{2}", version.Major, version.Minor, version.Build);
             return string.Format("{0} {1}", WindowTitle, versionText);
