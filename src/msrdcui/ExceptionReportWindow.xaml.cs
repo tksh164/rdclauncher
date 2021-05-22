20ec5195b4fca1ebf294cef5ba7b09dc66b6cb7d
Takeshi Katano <takatano@outlook.com>
2021-05-22 23:48:31 +0900

    Add ExceptionReportWindow

---
+++ src/msrdcui/ExceptionReportWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rdclauncher
+{
+    /// <summary>
+    /// Interaction logic for ExceptionReportWindow.xaml
+    /// </summary>
+    public partial class ExceptionReportWindow : Window
+    {
+        public ExceptionReportWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
