e7761539df0d8d65a7a847efc5d1f690f53b73ae
Takeshi Katano <takatano@outlook.com>
2021-05-23 02:46:06 +0900

    Update code behind

--- src/msrdcui/ExceptionReportWindow.xaml.cs
+++ src/msrdcui/ExceptionReportWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace rdclauncher
 {
@@ -19,9 +7,18 @@ namespace rdclauncher
     /// </summary>
     public partial class ExceptionReportWindow : Window
     {
-        public ExceptionReportWindow()
+        public ExceptionReportWindow(string reportText)
         {
             InitializeComponent();
+            DataContext = GetInitializedViewModel(reportText);
+        }
+
+        private ExceptionReportWindowViewModel GetInitializedViewModel(string reportText)
+        {
+            return new ExceptionReportWindowViewModel(this.Close)
+            {
+                ExceptionReportText = reportText,
+            };
         }
     }
 }
