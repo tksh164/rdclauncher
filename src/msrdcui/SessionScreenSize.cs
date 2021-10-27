3969b5e7bd20442a8dfedf23d5fa2d362a9e2271
Takeshi Katano <takatano@outlook.com>
2021-10-28 01:53:12 +0900

    Add SessionScreenSize class

---
+++ src/msrdcui/SessionScreenSize.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace rdclauncher
+{
+    public sealed class SessionScreenSize
+    {
+        public const int MinimumResolution = 200;
+        public const int MaximumResolution = 8192;
+        public const int UnsetResolutionValue = -1;
+
+        public SessionScreenSize()
+        {
+            ResolutionWidth = UnsetResolutionValue;
+            ResolutionHeight = UnsetResolutionValue;
+        }
+
+        public SessionScreenSize(int width, int height)
+        {
+            ResolutionWidth = width;
+            ResolutionHeight = height;
+        }
+
+        private int _resolutionWidth;
+        public int ResolutionWidth
+        {
+            get => _resolutionWidth;
+            private set
+            {
+                if (value != UnsetResolutionValue && (value < MinimumResolution || value > MaximumResolution))
+                {
+                    throw new ArgumentOutOfRangeException("value", value, string.Format("The valid resolution value is between {0} and {1}.", MinimumResolution, MaximumResolution));
+                }
+                _resolutionWidth = value;
+            }
+        }
+
+        private int _resolutionHeight;
+        public int ResolutionHeight
+        {
+            get => _resolutionHeight;
+            private set
+            {
+                if (value != UnsetResolutionValue && (value < MinimumResolution || value > MaximumResolution))
+                {
+                    throw new ArgumentOutOfRangeException("value", value, string.Format("The valid resolution value is between {0} and {1}.", MinimumResolution, MaximumResolution));
+                }
+                _resolutionHeight = value;
+            }
+        }
+    }
+}
