cc73da200635f8d285ba0f073551570e248605ee
Takeshi Katano <takatano@outlook.com>
2021-10-30 02:38:52 +0900

    Add session screen size initialization

--- src/msrdcui/Views/MainWindow.xaml.cs
+++ src/msrdcui/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Collections.ObjectModel;
 using rdclauncher.ViewModels;
 
 namespace rdclauncher.Views
@@ -36,6 +37,19 @@ namespace rdclauncher.Views
             {
                 vm.RdcWindowTitleHistory.Add(historyItem);
             }
+
+            vm.SelectedSessionScreenSize = new SessionScreenSize();
+            vm.SessionScreenSizeList = new ObservableCollection<SessionScreenSize>
+            {
+                vm.SelectedSessionScreenSize,
+                new SessionScreenSize(2560, 1440),
+                new SessionScreenSize(1920, 1200),
+                new SessionScreenSize(1920, 1080),
+                new SessionScreenSize(1600, 1200),
+                new SessionScreenSize(1280, 1024),
+                new SessionScreenSize(1024, 768),
+                new SessionScreenSize(800, 600)
+            };
 
             return vm;
         }
